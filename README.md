22e05f83206aab7d55dc3cc9ae964fdbcbe5a955
Jennifer Truong <56178065+jenjentro@users.noreply.github.com>
2022-06-24 12:06:42 -0700

    Tried to write an R script to unzip files, didn't work

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 June 17, 2022
 
 ## Cyclistic Bike Share Data Analysis
+
+### Data Management
+
+Data is currently stored in raw_data subfolder. Any files related to the data analysis project itself will be saved in a GitHub repository.
--- data-cleaning.R
+++ data-cleaning.R
@@ -2,3 +2,17 @@
 
 # Attach packages ----
 library(tidyverse)
+
+# Original working directory = "C:/Users/jenni/OneDrive/Documents/DataScience/Google Data Analytics Certificate/Capstone Project/google_capstone_casestudy"
+
+# Unzip files automatically ----
+#setwd("C:/Users/jenni/OneDrive/Documents/DataScience/Google Data Analytics Certificate/Capstone Project/google_capstone_casestudy/raw_data")
+
+#files <- list.files(path="../raw_data", pattern=".zip$")
+#out_dir <- "../raw_data"
+
+#for (i in files) {
+  #unzip(paste0("../raw_data/", i), exdir=out_dir)
+#}
+
+# Resource: https://stackoverflow.com/questions/33203800/unzip-a-zip-file
